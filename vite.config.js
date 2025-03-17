02aba0fcbd7b55a54718bf1b4c3b828e2391692f
Retopia <prestonltang@nyu.edu>
2025-03-17 15:43:53 -0400

    Changed sitmap generation

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import Sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
-  tailwindcss(),
-  Sitemap({
-    hostname: 'https://big2.live',
-    lastmod: new Date().toISOString(),
-    dynamicRoutes: ['/'],
-    readable: true,
-    outDir: 'dist',
-    changefreq: 'weekly',
-    priority: 1.0
-  })],
+  tailwindcss()],
 })
